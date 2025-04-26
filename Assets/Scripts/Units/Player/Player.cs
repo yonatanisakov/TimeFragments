8e2222a1b7ddcee4a67221adc5b6c18b2ca31ef9
Yoni <yonatan156354@gmail.com>
2025-04-26 09:14:38 +0300

    edited player inital location and add parameter of speed to control at the editor

--- Assets/Scripts/Units/Player/Player.cs
+++ Assets/Scripts/Units/Player/Player.cs
@@ -4,11 +4,12 @@ using Zenject;
 public class Player : MonoBehaviour
 {
     private PlayerController playerController;
+    [SerializeField] private float speed = 5;
 
     [Inject]
     public void Construct(IPlayerInput playerInput)
     {
-        playerController = new PlayerController(playerInput, transform);
+        playerController = new PlayerController(playerInput, transform,speed);
     }
     // Update is called once per frame
     void Update()
